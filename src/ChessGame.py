c61ab62849f90539c03931895d92c7617ed45d29
MrSofer <tal.sofer.winner@gmail.com>
2026-03-08 18:46:14 +0200

    Added Checks and Pins
    + clarity changes & turn from 0/1 to 1/-1

--- src/ChessGame.py
+++ src/ChessGame.py
@@ -1,4 +1,5 @@
 import fastapi, uvicorn
+from networkx.utils.rcm import pseudo_peripheral_node
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
 
@@ -17,6 +18,23 @@ class SelectHandler(BaseModel):
 class MoveHandler(BaseModel):
     moving_piece : int
     moving_to : int
+
+white_turn = 1
+black_turn = -1
+
+empty_square = 0
+pawn = white_pawn = 1
+knight = white_knight = 2
+bishop = white_bishop = 3
+rook = white_rook = 4
+queen = white_queen = 5
+king = white_king = 6
+black_pawn = -1
+black_knight = -2
+black_bishop = -3
+black_rook = -4
+black_queen = -5
+black_king = -6
 
 pieces_translation = {
 
@@ -51,128 +69,197 @@ numbers_translation = {
     -6:"k"
 }
 
-class Check:
-    def __init__(self ,attacker , attackerPos ,kingPos):
-        self.attacker = attacker
-        self.attackerPos = attackerPos
-        self.kingPos = kingPos
+knight_moves = [(-1,-2),(-1,2),(-2,-1),(-2,1),(1,-2),(1,2),(2,-1),(2,1)]
 
-    """def find_available_squares(self) -> list():
-        ret = self.attackerPos()
-        match abs(self.attacker):
-            case 3:
-            
-            case 4:
+bishop_directions = [(-1,-1),(-1,1),(1,-1),(1,1)]
 
-            case 5:
-            
-        return ret"""
+rook_directions = [(-1, 0),(0, -1),(1, 0),(0, 1)]
 
-def coord_to_ind(i: int, j: int) -> int:
-    return 8*i + j
+queen_directions = king_moves = [(-1,-1),(-1,1),(1,-1),(1,1),(-1, 0),(0, -1),(1, 0),(0, 1)]
+
+moves = [(knight, knight_moves), (king, king_moves)]
+directions = [(bishop, bishop_directions), (rook,rook_directions)]
+
+ROWS = COLS = 8
+BOARD_LEN = 64
+
+
+def coord_to_ind(row: int, col: int) -> int:
+    return ROWS*row + col
 
 def ind_to_coord(ind: int) -> tuple[int,int]:
-    return ind // 8 , ind % 8
+    return ind // ROWS , ind % COLS
 
 
 class ChessGame:
+
+    board : list[int]
+    turn : int
+    is_check : bool
+    kings : list[int]
+    can_castle = list[list[bool]]
+
+
     def __init__(self):
-        self.board = [4,2,3,5,6,3,2,4,
-                      1,1,1,1,1,1,1,1,
-                      0,0,0,0,0,0,0,0,
-                      0,0,0,0,0,0,0,0,
-                      0,0,0,0,0,0,0,0,
-                      0,0,0,0,0,0,0,0,
-                      -1,-1,-1,-1,-1,-1,-1,-1,
-                      -4,-2,-3,-5,-6,-3,-2,-4]
-        self.turn = 0 #0 is white's turn, 1 is black's turn
-        self.is_in_check = False
-        self.can_castle = [[True,True],[True,True]]
-        self.pinned_pieces = set()
+        self.board = [white_rook,white_knight,white_bishop,white_queen,white_king,white_bishop,white_knight,white_rook,
+                      white_pawn,white_pawn,white_pawn,white_pawn,white_pawn,white_pawn,white_pawn,white_pawn,
+                      empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,
+                      empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,
+                      empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,
+                      empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,empty_square,
+                      black_pawn,black_pawn,black_pawn,black_pawn,black_pawn,black_pawn,black_pawn,black_pawn,
+                      black_rook,black_knight,black_bishop,black_queen,black_king,black_bishop,black_knight,black_rook]
+        self.turn = white_turn #1 is white's turn, -1 is black's turn
+        self.is_check = False
+        self.kings = [-1,4,60]
+        self.can_castle = [[],[True,True],[True,True]]
         self.en_passant = None
+        
+    def is_king_safe(self,king_ind) -> bool:
+        
+        king_row,king_col = ind_to_coord(king_ind)
 
-    def find_legal_moves(self,ind) -> list[int]: #todo: add checks, en passant, castling,promotion
-        ret = list()
-        piece_type = self.board[ind]
-        i,j = ind_to_coord(ind)
-        directions = []
-        moves = []
-        color_sign = 1 - 2*self.turn
-        if self.is_in_check:
-            pass #todo after implementing checks
+        pawn_take_from_left = coord_to_ind(king_row + self.turn, king_col-1)
+        pawn_take_from_right = coord_to_ind(king_row + self.turn, king_col+1)
+        if ((king_col > 0) and (-1 * self.turn * self.board[pawn_take_from_left] == pawn)) or ((king_col < COLS - 1) and (-1 * self.turn * self.board[pawn_take_from_right] == pawn)): #an opposite color pawn can take the king
+            return False
+
+        for piece,piece_moves in moves:
+            for k,l in piece_moves:
+                curr_ind = coord_to_ind(king_row + k, king_col + l)
+                if (0 <= king_row + k < ROWS) and (0 <= king_col + l < COLS) and (-1 * self.turn * self.board[curr_ind] == piece): #an opposite color knight/king can take the king
+                    return False
+
+        for piece,piece_directions in directions: #queen shares sliding vectors with rook and bishop, so no need to add a queen loop
+            for k,l in piece_directions:
+                m = king_row + k
+                n = king_col + l
+                while (0 <= m < ROWS) and (0 <= n < COLS):
+                    curr_ind = coord_to_ind(m, n)
+                    if -1 * self.turn * self.board[curr_ind] in [piece,queen]: #an opposite color bishop/rook/queen can take the king
+                        return False
+                    elif self.board[curr_ind] != empty_square: #a non-threatening piece is blocking the way in that direction
+                        break
+                    m += k
+                    n += l
+
+        return True
+
+    def pseudo_move(self, ind, towards) -> tuple[int,bool]:
+        captured_piece = self.board[towards]
+        self.board[towards] = self.board[ind]
+        self.board[ind] = empty_square
+        is_en_passant = False
+        if  self.turn * self.board[towards] == pawn and self.en_passant == towards:
+            self.board[towards - ROWS * self.turn] = empty_square
+            captured_piece = - self.turn * pawn
+            is_en_passant = True
+
+        if self.turn * self.board[towards] == king:
+            self.kings[self.turn] = towards
+
+        return captured_piece,is_en_passant
+
+    def unmove(self, current_ind, original_ind, piece_captured, was_en_passant):
+        self.board[original_ind] = self.board[current_ind]
+        if was_en_passant:
+            self.board[current_ind] = empty_square
+            self.board[current_ind - self.turn * ROWS] = piece_captured
         else:
-            if color_sign * piece_type <= 0:
-                return ret
-            match abs(piece_type):
-                case 1:
-                    step_one = coord_to_ind(i+color_sign,j)
-                    if self.board[step_one] == 0:
-                        ret.append(step_one)
-                        step_two = coord_to_ind(i+2*color_sign,j)
-                        if (i == 1 + (5 * self.turn)) and (self.board[step_two] == 0):
-                            ret.append(step_two)
-                    if j > 0:
-                        take_left = step_one - 1
-                        if color_sign * self.board[take_left] < 0 or take_left == self.en_passant:
-                            ret.append(take_left)
-                    if j < 7:
-                        take_right = step_one + 1
-                        if color_sign * self.board[take_right] < 0 or take_right == self.en_passant:
-                            ret.append(take_right)
-                case 2:
-                    moves = [(-1,-2),(-1,2),(-2,-1),(-2,1),(1,-2),(1,2),(2,-1),(2,1)]
-                case 3:
-                    directions = [(-1,-1),(-1,1),(1,-1),(1,1)]
-                case 4:
-                    directions = [(-1, 0),(0, -1),(1, 0),(0, 1)]
-                case 5:
-                    directions = [(-1,-1),(-1,1),(1,-1),(1,1),(-1, 0),(0, -1),(1, 0),(0, 1)]
-                case 6:
-                    moves = [(-1,-1),(-1,1),(1,-1),(1,1),(-1, 0),(0, -1),(1, 0),(0, 1)]
+            self.board[current_ind] = piece_captured
+
+        if self.turn * self.board[original_ind] == king:
+            self.kings[self.turn] = original_ind
+
+    def king_safety_check(self,ind,towards,legal_moves_list) -> None: #using the fact that lists in python are mutable
+        piece_captured,is_en_passant = self.pseudo_move(ind,towards)
+        if self.is_king_safe(self.kings[self.turn]):
+            legal_moves_list.append(towards)
+        self.unmove(towards,ind,piece_captured,is_en_passant)
 
 
-            if len(directions) > 0:
-                for k, l in directions:
-                    m = i + k
-                    n = j + l
-                    while (0 <= m <= 7) and (0 <= n <= 7):
-                        curr_ind = coord_to_ind(m, n)
-                        print(curr_ind)
-                        if self.board[curr_ind] == 0:
-                            ret.append(curr_ind)
-                        else:
-                            if color_sign * self.board[curr_ind] < 0:
-                                ret.append(curr_ind)
-                            break
-                        m += k
-                        n += l
+    def find_legal_moves(self,ind) -> list[int]: #todo: add castling, promotion
+        ret = list()
+        piece_type = self.turn * self.board[ind]
+        piece_row,piece_col = ind_to_coord(ind)
+        legal_directions = []
+        legal_moves = []
+        match piece_type:
+            case 1: #pawn
+                step_one = coord_to_ind(piece_row+self.turn,piece_col)
+                if self.board[step_one] == empty_square:
+                    self.king_safety_check(ind,step_one,ret)
+                    step_two = coord_to_ind(piece_row+ 2*self.turn ,piece_col)
+                    if (piece_row == 1 + (5 * int(self.turn == black_turn))) and (self.board[step_two] == empty_square):
+                        self.king_safety_check(ind,step_two,ret)
+                if piece_col > 0:
+                    take_left = step_one - 1
+                    if self.turn * self.board[take_left] < 0 or take_left == self.en_passant:
+                        self.king_safety_check(ind,take_left,ret)
+                if piece_col < COLS-1:
+                    take_right = step_one + 1
+                    if self.turn * self.board[take_right] < 0 or take_right == self.en_passant:
+                        self.king_safety_check(ind,take_right,ret)
+            case 2: #knight
+                legal_moves = knight_moves
+            case 3: #bishop
+                legal_directions = bishop_directions
+            case 4: #rook
+                legal_directions = rook_directions
+            case 5: #queen
+                legal_directions = queen_directions
+            case 6: #king
+                legal_moves = king_moves
 
-            elif len(moves) > 0:
-                for k, l in moves:
-                    curr_ind = coord_to_ind(i+k,j+l)
-                    if (0 <= i + k <= 7) and (0 <= j + l <= 7) and (color_sign * self.board[curr_ind] <= 0):
-                        ret.append(curr_ind)
+
+        if len(legal_directions) > 0:
+            for k, l in legal_directions:
+                m = piece_row + k
+                n = piece_col + l
+                while (0 <= m < ROWS) and (0 <= n < COLS):
+                    curr_ind = coord_to_ind(m, n)
+                    if self.board[curr_ind] == empty_square:
+                        self.king_safety_check(ind,curr_ind,ret)
+                    else:
+                        if self.turn * self.board[curr_ind] < 0:
+                            self.king_safety_check(ind,curr_ind,ret)
+                        break
+                    m += k
+                    n += l
+
+        elif len(legal_moves) > 0:
+            for k, l in legal_moves:
+                curr_ind = coord_to_ind(piece_row+k,piece_col+l)
+                if (0 <= piece_row + k < ROWS) and (0 <= piece_col + l < COLS) and (self.turn * self.board[curr_ind] <= 0):
+                    self.king_safety_check(ind,curr_ind,ret)
+
         return ret
 
     def move(self,piece:int,towards:int) -> str:
-        color_sign = 1 - 2*self.turn
-        captures = self.board[towards] != ""
+        captures = self.board[towards] != empty_square
         self.board[towards] = self.board[piece]
-        self.board[piece] = 0
-        if self.en_passant == towards:
-            self.board[towards - 8*color_sign] = 0
+        self.board[piece] = empty_square
+        if (self.turn * self.board[towards] == pawn) and (self.en_passant == towards):
+            self.board[towards -  self.turn * ROWS] = empty_square
             captures = True
-        if abs(self.board[towards]) == 1 and towards // 8 == 3 + self.turn:
-            self.en_passant = towards - 8*color_sign
+        if self.turn * self.board[towards] == pawn and towards // ROWS == 3 + int(self.turn == black_turn):
+            self.en_passant = towards - ROWS * self.turn
         else:
             self.en_passant = None
-        self.turn = 1 - self.turn
-        return str.upper(numbers_translation[self.board[towards]]) + ("x" if captures else "") + chr(97 + towards // 8) + str(towards % 8)
+
+        if self.turn * self.board[towards] == king:
+            self.kings[self.turn] = towards
+
+        self.turn = -1 * self.turn
+        return str.upper(numbers_translation[self.board[towards]]) + ("x" if captures else "") + chr(97 + towards % COLS) + str((towards // ROWS) + 1)
+
+
+
 
 game : ChessGame = ChessGame()
 
 @app.get("/reset")
-async def resetGame():
+async def reset_game():
     global game
     game = ChessGame()
 
@@ -192,10 +279,8 @@ async def move(request:MoveHandler) -> dict:
 
 """
 todo:
-implement en passant
 implement pins
 implement castling
 implement checks
-make sure board is aligned with front
-make sure board refreshes when page refreshes 
+implement promotion
 """
